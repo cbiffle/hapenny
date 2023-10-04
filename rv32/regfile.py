4748f5b453696ae01b4a8768eb7d43b5cb8beb4d
Cliff L. Biffle <code@cliffle.com>
2023-10-04 09:07:17 -0700

    regfile: add rv32e, split RAM into 16 bit halves
    This makes the instance naming somewhat easier to follow, IMO.

--- rv32/regfile.py
+++ rv32/regfile.py
@@ -13,28 +13,49 @@ class RegFile(Component):
         'value': Out(32),
     })))
 
+    def __init__(self, *, rv32e = False):
+        super().__init__()
+
+        self.rv32e = rv32e
+
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.mem = mem = Memory(
-            width = 32,
-            depth = 32,
-            name = "regfile",
+        nregs = 16 if self.rv32e else 32
+
+        m.submodules.mem_hi = mem_hi = Memory(
+            width = 16,
+            depth = nregs,
+            name = "regfile_hi",
         )
 
-        rp = mem.read_port(transparent = False)
-        wp = mem.write_port()
+        rp_hi = mem_hi.read_port(transparent = False)
+        wp_hi = mem_hi.write_port()
+
+        m.submodules.mem_lo = mem_lo = Memory(
+            width = 16,
+            depth = nregs,
+            name = "regfile_lo",
+        )
+
+        rp_lo = mem_lo.read_port(transparent = False)
+        wp_lo = mem_lo.write_port()
 
         m.d.comb += [
-            rp.addr.eq(self.read_cmd.payload),
-            rp.en.eq(self.read_cmd.valid),
+            rp_lo.addr.eq(self.read_cmd.payload),
+            rp_hi.addr.eq(self.read_cmd.payload),
+            rp_lo.en.eq(self.read_cmd.valid),
+            rp_hi.en.eq(self.read_cmd.valid),
 
-            self.read_resp.eq(rp.data),
+            self.read_resp.eq(Cat(rp_lo.data, rp_hi.data)),
 
-            wp.addr.eq(self.write_cmd.payload.reg),
-            wp.data.eq(self.write_cmd.payload.value),
+            wp_lo.addr.eq(self.write_cmd.payload.reg),
+            wp_hi.addr.eq(self.write_cmd.payload.reg),
+            wp_lo.data.eq(self.write_cmd.payload.value[:16]),
+            wp_hi.data.eq(self.write_cmd.payload.value[16:]),
             # Block writes to x0.
-            wp.en.eq((self.write_cmd.payload.reg != 0) & self.write_cmd.valid),
+            wp_lo.en.eq((self.write_cmd.payload.reg != 0) & self.write_cmd.valid),
+            wp_hi.en.eq((self.write_cmd.payload.reg != 0) & self.write_cmd.valid),
         ]
 
         return m
