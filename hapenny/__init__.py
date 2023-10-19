06e16884232ff40cdce1c955e9745a55905fa6ad
Cliff L. Biffle <code@cliffle.com>
2023-10-19 11:34:28 -0700

    Simplify and improve onehot_choice and oneof.
    - Internals made simpler using lists and reduce
    - Both can now accept an optional default value for fallback if no arms
      match.

--- hapenny/__init__.py
+++ hapenny/__init__.py
@@ -3,6 +3,8 @@ from amaranth.lib import data, enum, wiring
 from amaranth.lib.enum import Enum
 from amaranth.lib.wiring import In, Out
 
+from functools import reduce
+
 class StreamSig(wiring.Signature):
     def __init__(self, payload_shape):
         super().__init__({
@@ -39,30 +41,51 @@ def mux(select, one, zero):
 # signal.
 #
 # onehot_sig should be a signal of N bits, and options should be a dict with at
-# most N entries. Each key in the dict is a bit number in onehot_sig, and the
-# corresponding value will be produced as output.
+# most N entries. Each key in the dict is a bit number in onehot_sig, or a
+# tuple of bit numbers, and the corresponding value will be produced as output
+# when the indicated bit(s) are set in the state.
 #
-# If the onehot invariant is violated and more than one bit in onehot_sig is 1,
-# this will OR together the results. This is often cheaper than using explicit
-# muxes on 4LUT devices.
-def onehot_choice(onehot_sig, options):
+# If a default is provided, it will be used if none of the explicit conditions
+# in the options map fires. By default, the default is zero.
+#
+# This assumes all bits in the onehot_sig are mutually exclusive, and combines
+# each path using a bitwise OR instead of muxes, which is often cheaper on 4LUT
+# devices. However, this means if the onehot invariant is violated, you'll get
+# nonsense output. If that concerns you, see oneof instead.
+def onehot_choice(onehot_sig, options, default = None):
     assert len(options) > 0
-    output = None
+    output = []
+    matches = []
     for (choice, result) in options.items():
         if isinstance(choice, Enum):
             choice = choice.value
+        if isinstance(choice, list) or isinstance(choice, tuple):
+            pass
+        else:
+            # Force choice to be a sequence
+            choice = [choice]
         if isinstance(result, Enum):
             result = result.value
         if isinstance(result, int):
             result = Const(result)
 
-        case = onehot_sig[choice].replicate(result.shape().width) & result
-        if output is not None:
-            output = output | case
-        else:
-            output = case
+        condition = reduce(lambda a, b: a | b, map(lambda s: onehot_sig[s],
+                                                   choice))
+        matches.append(condition)
 
-    return output
+        case = condition.replicate(result.shape().width) & result
+
+        output.append(case)
+
+    if default is not None:
+        if isinstance(default, Enum):
+            default = default.value
+        if isinstance(default, int):
+            default = Const(default)
+        no_match = ~reduce(lambda a, b: a | b, matches)
+        output.append(no_match.replicate(default.shape().width) & default)
+
+    return reduce(lambda a, b: a | b, output)
 
 # Builds a chained mux that selects between a set of options, which must be
 # mutually exclusive.
@@ -71,39 +94,41 @@ def onehot_choice(onehot_sig, options):
 # boolean condition. If 1, the second element is OR'd into the result.
 #
 # This means if more than one condition is true simultaneously, the result will
-# bitwise OR the results together.
+# bitwise OR the results together. It is up to you to ensure that all
+# conditions are mutually exclusive.
+#
+# If a default is provided, it will be used when no other conditions match.
+# Otherwise, the default is zero.
 #
 # If you've got a onehot control signal instead of a bunch of separate condition
 # strobes, see onehot_choice.
 def oneof(options, default = None):
     assert len(options) > 0
-    output = None
-    any_match = None
+    output = []
+    matches = []
     for (condition, result) in options:
         if isinstance(condition, int):
-            condition = Const(result)
+            condition = Const(condition)
         if isinstance(result, Enum):
             result = result.value
         if isinstance(result, int):
             result = Const(result)
         
-        if any_match is not None:
-            any_match = any_match | condition.any()
-        else:
-            any_match = condition.any()
+        matches.append(condition.any())
 
         case = condition.any().replicate(result.shape().width) & result
 
-        if output is not None:
-            output = output | case
-        else:
-            output = case
+        output.append(case)
 
     if default is not None:
-        no_match = ~any_match
-        output = output | (no_match.replicate(default.shape().width) & default)
+        if isinstance(default, Enum):
+            default = default.value
+        if isinstance(default, int):
+            default = Const(default)
+        no_match = ~reduce(lambda a, b: a|b, matches)
+        output.append(no_match.replicate(default.shape().width) & default)
 
-    return output
+    return reduce(lambda a, b: a|b, output)
 
 def hihalf(signal):
     return signal[16:]
